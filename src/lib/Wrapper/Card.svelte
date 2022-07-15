9e77a41fe60725891c263d220b6f3dee40857cf8
Jonas <jonas-focke@mailbox.org>
2022-07-15 11:04:13 +0200

    made card component more configurable

--- src/lib/Wrapper/Card.svelte
+++ src/lib/Wrapper/Card.svelte
@@ -1,9 +1,25 @@
 <!-- @component
   a simple wrapper to display the content in a cardlike container
+
+  **padding**
+  * tailwind padding class
+  * default: ''
+  * the padding of the card container
+  * example: 'p-4'
+
+  **rounded**
+  * boolean
+  * default: false
+  * if the card should have rounded corners
  -->
 <script lang="ts">
+  export let padding:
+    | `${'' | '-'}p${'' | 't' | 'b' | 'r' | 'l' | 'x' | 'y'}-${number}`
+    | '' = '';
+
+  export let rounded: boolean = false;
 </script>
 
-<div class="bg-surface p-5 rounded-xl shadow-lg">
+<div class="bg-surface {padding} shadow-lg" class:rounded-xl={rounded}>
   <slot />
 </div>
