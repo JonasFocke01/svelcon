550d78bdfcb02fce547ec41454d8d8e7b7220155
Jonas <jonas-focke@mailbox.org>
2022-07-19 18:09:05 +0200

    introduced a badge component

---
+++ src/lib/Wrapper/Badge.svelte
@@ -0,0 +1,47 @@
+<!-- @component
+  a basic badge
+
+  **text**
+    * string
+    * default: ''
+    * the text of the badge. 
+    * This should never be a long text but rather a single digit
+
+  **bgColor**
+    * string
+    * default: 'bg-accent'
+    * sets the bgColor of the badge
+
+  **slot**
+    * slot
+    * default: !NO DEFAULT!
+    * what to display the badge on
+ -->
+<script lang="ts">
+  import Text from './Text.svelte';
+
+  export let text: string = '';
+
+  export let bgColor:
+    | 'error'
+    | 'primary'
+    | 'secondary'
+    | 'accent'
+    | 'success'
+    | 'warning'
+    | 'info'
+    | 'surface'
+    | 'text'
+    | 'black' = 'accent';
+</script>
+
+<div class="relative max-w-fit ">
+  <div class="absolute w-full -mt-2">
+    <div class="ml-auto px-1 rounded-full max-w-min bg-{bgColor}">
+      <div class="translate-x-0.5">
+        <Text {text} size="small" />
+      </div>
+    </div>
+  </div>
+  <slot />
+</div>
